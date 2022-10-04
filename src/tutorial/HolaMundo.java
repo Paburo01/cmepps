458dccbec432e39178310041ee9660c45fc3bd4d
usuario <usuario@pc21-086>
2022-10-04 17:15:33 +0200

    SISISISISISISISISISI

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -6,7 +6,11 @@ public class HolaMundo {
 		System.out.println("hola mundo");
 		String name="Pablo";
 		System.out.println("BIENVENIDO, "+name);
-
+		int i = 0;
+		for (;;) {
+			System.out.println(i);
+			i++;
+		}
 	}
 
 }
